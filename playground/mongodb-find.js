cdab69e61f1532fbfd87f8a6ca06634f165bb358
Sachin Aggarwal <sachinaggarwal@Sachins-MacBook-Air.local>
2018-01-09 00:49:57 +0530

    Fetching completed

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -7,7 +7,9 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,database)=>{
   return console.log('unable to connect');
   console.log('Connected to Mongodb server');
 
-  db.collection('Todos').find().toArray().then((docs)=>{
+  db.collection('Todos').find({
+    _id: new ObjectID('5a52fe3c9bb8d7545deab99b')  //we used completed:false before this
+  }).toArray().then((docs)=>{
     console.log('Todos');
     console.log(JSON.stringify(docs,undefined,2));
   },(err)=>{
