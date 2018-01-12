de6393204778ba3b40a57b58c46ad9d386179756
Sachin Aggarwal <sachinaggarwal@Sachins-MacBook-Air.local>
2018-01-13 01:50:53 +0530

    validations

--- server/server.js
+++ server/server.js
@@ -5,22 +5,27 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo = mongoose.model('Todo',{
   text : {
-    type: String
+    type: String,
+    required : true,
+    minlength : 1,
+    trim : true
   },
   completed: {
-    type : Boolean
+    type : Boolean,
+    default : false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default : null
   }
 });
 
 var newTodo = new Todo({
-  text : 'Cook'
+  text : '  sachin  '
 });
 
 newTodo.save().then((savedDoc)=>{
   console.log(savedDoc);
 }, (e)=>{
-console.log('Unable to save todo');
+console.log(e);
 });
