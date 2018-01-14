6814e4deb982ae8d4dc2db993a2004b968a4aa65
codingblazer <sasachin04@gmail.com>
2018-01-15 04:05:57 +0530

    Sets up finding particular todo route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todos.js');
@@ -24,14 +25,32 @@ todo.save().then((doc)=>{
 app.get('/todos',(req,res)=>{
 Todo.find().then(
   (todos)=>{
-  res.send({todos});},
+  res.send({todos})},
   (e)=>{
   res.status(400).send(e);
 })
 });
 
+app.get('/todos/:id',(req,res)=>{
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send("Object id");
+  }
+
+  Todo.findById(id).then((todo)=>{
+    if(!todo)
+    return res.status(404).send("not present");
+    else
+    res.send({todo});
+  }).catch((e)=>{
+    res.status(400).send(e);
+  });
+}
+);
+
 app.listen(3000, ()=>{
   console.log('started on port 3000');
 });
+
 
 module.exports = {app};
