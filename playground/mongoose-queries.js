a071ec4a2c93118d81b5a75d0d29a903e7fbb895
codingblazer <sasachin04@gmail.com>
2018-01-15 02:57:49 +0530

    ID validation

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,11 +1,16 @@
 const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todos');
+const {ObjectID} = require('mongodb');
 
 var id = '5a5bb6f62b6c36072a1d5f9d';
 
+if(!ObjectID.isValid(id))
+console.log('id is not valid');
+
+
 //simple find command with query asking for specific id
 Todo.find({
-  _id: id
+  _id: id //mongoose doesn't need us to create new ID()
 }).then((todos)=>{
   console.log('Todos',todos);
 })
