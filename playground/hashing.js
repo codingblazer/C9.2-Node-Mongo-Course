b18cd3cd1986aec922ea079a075f10b9fda1ec46
codingblazer <sasachin04@gmail.com>
2018-01-19 00:09:55 +0530

    Adds intro to hash tokenning using jwt

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,20 +1,9 @@
-const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
 
-var message = 'I am user number 3';
-var hash = SHA256(message).toString();
+var data = {id:10};
 
-console.log('Message is :'+message);
-console.log('Hash is :'+hash);
+var token = jwt.sign(data,'123secret');
+console.log(token);
 
-var data = { id: 4};
-var token = {data,hash:SHA256(JSON.stringify(data)+'somesecret').toString()};
-
-// token.data.id = 5;
-// token.hash= SHA256(JSON.stringify(token.data)).toString();
-
-var resultHash = SHA256(JSON.stringify(token.data)+'somesecret').toString();
-
-if(resultHash === token.hash)
-console.log('Data was not changed');
-else
-console.log('Data was changed');
+var decoded = jwt.verify(token,'123secret');
+console.log('data: '+JSON.stringify(decoded));
