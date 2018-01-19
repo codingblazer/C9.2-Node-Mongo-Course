993b0455697f3c5b541bda034f93a077d1c9eada
codingblazer <sasachin04@gmail.com>
2018-01-19 20:11:14 +0530

    Adds passwords in hashed form in db and pre hook middleware in mongoose

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,9 +1,26 @@
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
+//
+// var data = {id:10};
+//
+// var token = jwt.sign(data,'123secret');
+// console.log(token);
+//
+// var decoded = jwt.verify(token,'123secret');
+// console.log('data: '+JSON.stringify(decoded));
 
-var data = {id:10};
+const bcrypt = require('bcrypt');
+var password = '123abc';
+var hashedPass;
 
-var token = jwt.sign(data,'123secret');
-console.log(token);
+//bcrypt contains diff methods calle salts like general salt => first arg are the number of rounds => more rounds, more difficul to crack hut also take more time to apply
+bcrypt.genSalt(10,(err,salt)=>{
+  bcrypt.hash(password,salt,(err,hash)=>{ //give plaintext, salt method and you will get callback with the hashed pass
+    console.log(hash);
+    hashedPass = hash;
+  });
+});
 
-var decoded = jwt.verify(token,'123secret');
-console.log('data: '+JSON.stringify(decoded));
+//this funciton will be used by us => we will take plaintexxt pass from user and compate it with the hash pass which is stored in our database using this func
+bcrypt.compare('123abc',hashedPass,(err,res)=>{
+  console.log(res); //will be true or false
+});
